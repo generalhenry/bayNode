f4690c1b7e3f146d4cc1e0b3fe53051a3dd79c34
Henry Allen-Tilford <htilford@gmail.com>
2011-08-18 03:25:11 +0000

    commit!

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 var express = require('express');
+var io = require('socket.io');
 
 var app = express.createServer();
+
+app.configure(function(){
+    app.use(express.methodOverride());
+    app.use(express.bodyParser());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
+});
 
 app.get('/', function (req,res) {
   res.send('hello bayNode'); 
 });
 
 app.listen( process.env.C9_PORT || 93);
+
+var sockets = io.listen(app).sockets;
+
+sockets.on('connection', function (socket) {
+    socket.emit('news','NEWS!');
+});
